446e0ce31214e9d62c7bd001a00a32b097d44770
Veronica0420 <2311686624@qq.com>
2019-10-15 08:12:45 +0800

    我的书架（页面展示于分类）

--- app.js
+++ app.js
@@ -1,39 +1,158 @@
 //app.js
+var util = require('/utils/util.js');
+var api = require('/utils/api.js');
+
 App({
+  is_on_launch: true,
+
   onLaunch: function () {
+
     // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // console.log(wx.getSystemInfoSync());
+    // this.getStoreData();
+    //  this.getCatList();
+    var access_token = wx.getStorageSync("access_token");
+    // console.log('token:' + access_token);
+    //  if (access_token) {
+    //   this.globalData.is_auth = 1;
+    //}
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
-    wx.getSetting({
-      success: res => {
-        if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
+  },
 
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
+  //xqq:用户授权登陆
+  getToken: function () {
+    var promise = new Promise((resolve, reject) => {
+      var that = this;
+      wx.login({
+        success: function (res) {
+          if (res.code) {
+            var code = res.code;
+            wx.getUserInfo({
+              success: function (res) {
+                console.log('login.....');
+                getApp().request({
+                  url: api.applogin.login,
+                  method: "post",
+                  data: {
+                    code: code,
+                    userInfo: res.rawData,
+                    //   encrypted_data: res.encryptedData,
+                    //  iv: res.iv,
+                    //   signature: res.signature
+                  },
+                  success: function (res) {
+
+                    wx.setStorageSync("access_token", res.data.token);
+                    resolve(res)
+                  }
+                });
+              }
+            });
+          } else {
+            //console.log(res);
+          }
+
+        }
+      })
+    });
+    return promise;
+  },
+
+  //xqq:用户授权登陆
+  login: function () {
+    var promise = new Promise((resolve, reject) => {
+      var that = this;
+      wx.login({
+        success: function (res) {
+          if (res.code) {
+            var code = res.code;
+            getApp().request({
+              url: api.applogin.login1,
+              method: "post",
+              data: {
+                code: code,
+              },
+              success: function (res) {
+                if (res.data.result == 1) {
+                  wx.setStorageSync("access_token", res.data.token);
+                  getApp().globalData.access_token = res.data.token;
+                }
+                resolve(res)
+              }
+
+            });
+          } else {
+            //console.log(res);
+          }
+
+        }
+      })
+    });
+    return promise;
+  },
+
+  //xqq：request工具封装
+  request: function (object) {
+    var access_token = wx.getStorageSync("access_token");
+    //  console.log('token:' + access_token);
+    if (access_token) {
+      if (!object.data)
+        object.data = {};
+      object.data.access_token = access_token;
+    }
+    console.log('request.data', object.data)
+    wx.request({
+      url: object.url,
+      header: object.header || {
+        'content-type': 'application/x-www-form-urlencoded'
+      },
+      data: object.data || {},
+      method: object.method || "GET",
+      dataType: object.dataType || "json",
+      success: function (res) {
+        if (res.data.code == -1) {
+          getApp().login();
+        } else {
+          if (object.success)
+            object.success(res.data);
+        }
+      },
+      fail: function (res) {
+        var app = getApp();
+        if (app.is_on_launch) {
+          app.is_on_launch = false;
+          wx.showModal({
+            title: "网络请求出错",
+            content: res.errMsg,
+            showCancel: false,
+            success: function (res) {
+              if (res.confirm) {
+                if (object.fail)
+                  object.fail(res);
               }
             }
-          })
+          });
+        } else {
+          wx.showToast({
+            title: res.errMsg,
+            image: "/images/icon-warning.png",
+          });
+          if (object.fail)
+            object.fail(res);
         }
+      },
+      complete: function (res) {
+        if (object.complete)
+          object.complete(res);
       }
-    })
+    });
   },
+
   globalData: {
-    userInfo: null
-  }
+    loginCode: null,
+    access_token: null,
+    is_auth: 0    //登录后返回的授权状态：0未授权，1已授权
+  },
+
+
 })
